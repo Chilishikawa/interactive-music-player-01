254ffc29be572373b6247de9dd0a55e4d2cca143
Chilishikawa <chilishikawa13@gmail.com>
2025-09-09 05:14:52 -0300

    correción al nombre del delegado

--- InteractiveMusicPlayerApplication/MusicPlayer.cs
+++ InteractiveMusicPlayerApplication/MusicPlayer.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public delegate void SondEventHandler(string songTitle);
+public delegate void SongEventHandler(string songTitle);
 
 namespace InteractiveMusicPlayerApplication
 {
     public class MusicPlayer
     {
-        public event SondEventHandler SongPlayed;
-        public event SondEventHandler SongPaused;
-        public event SondEventHandler SongStopped;
-        public event SondEventHandler SongSkipped;
+        public event SongEventHandler SongPlayed;
+        public event SongEventHandler SongPaused;
+        public event SongEventHandler SongStopped;
+        public event SongEventHandler SongSkipped;
 
         public void Play(string songTitle) => SongPlayed?.Invoke(songTitle);
         public void Pause(string songTitle) => SongPaused?.Invoke(songTitle);
